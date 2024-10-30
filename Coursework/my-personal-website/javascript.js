1b48b797945dda1a892668113033ce80051132e2
John Doe <johndoe@email.com>
2024-10-29 21:05:35 -0400

    updates to website

--- Coursework/my-personal-website/javascript.js
+++ Coursework/my-personal-website/javascript.js
@@ -1,1 +1,7 @@
 console.log("Hello World");
+
+document.getElementById('form').addEventListener('submit', function (event) {
+    // Prevent form submission to allow validation or further actions if needed
+    event.preventDefault();
+    window.location.href = 'success.html';
+});
